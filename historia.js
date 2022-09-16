febc750dbef7d02fa9a032ea33ffa9f1159178c1
duarte <danilobrum889@gmail.com>
2022-09-16 13:55:13 -0300

    continuo a historia

--- historia.js
+++ historia.js
@@ -2,3 +2,5 @@
 vivia um macaco que morava em um pe de jabuticaba que ficava perto de um barranco, 
 onde vivia um crocodila e sua esposa.
     Um dia um crocodilo veio ate o barranco proximo ao rio e descansou embaixo da arvore.
+O bondoso macaco lhe ofereceu algumas frutas.
+     O crocodilo voltou no dia seguinte para ganhar mais fruta, e ele gostou muito.
