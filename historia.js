8e58d5c58d304f606c84ccaf05b2cff50aa51789
duarte <danilobrum889@gmail.com>
2022-09-16 13:47:57 -0300

    crio uma historia

---
+++ historia.js
@@ -0,0 +1,4 @@
+    Era uma vez, em uma floresta, 
+vivia um macaco que morava em um pe de jabuticaba que ficava perto de um barranco, 
+onde vivia um crocodila e sua esposa.
+    Um dia um crocodilo veio ate o barranco proximo ao rio e descansou embaixo da arvore.
